3100dd1e720eb1f1c095be60956e79ade0e641bc
Jim Barritt <jim@planet-ix.com>
2016-02-24 12:51:14 +0000

    Adding empty test and getting build working

---
+++ src/test/java/jmdbtutorial/crypto/TestCryptoHashing.java
@@ -0,0 +1,18 @@
+package jmdbtutorial.crypto;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.junit.Assert.fail;
+
+public class TestCryptoHashing {
+
+    Logger LOG = LoggerFactory.getLogger(TestCryptoHashing.class);
+
+    @Test
+    public void sha526_hash() {
+        LOG.info("Hello hashi!");
+
+    }
+}
