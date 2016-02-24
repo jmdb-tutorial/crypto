ac981923b583146e8f17bc848c443db1a08a6c4e
Jim Barritt <jim@planet-ix.com>
2016-02-24 16:45:27 +0000

    Printing out hashes

--- src/test/java/jmdbtutorial/crypto/TestCryptoHashing.java
+++ src/test/java/jmdbtutorial/crypto/TestCryptoHashing.java
@@ -1,18 +1,45 @@
 package jmdbtutorial.crypto;
 
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import static org.junit.Assert.fail;
+import javax.xml.bind.DatatypeConverter;
+import java.io.ByteArrayInputStream;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+import java.util.Base64;
+
+import static java.lang.System.out;
 
 public class TestCryptoHashing {
 
-    Logger LOG = LoggerFactory.getLogger(TestCryptoHashing.class);
-
     @Test
-    public void sha526_hash() {
-        LOG.info("Hello hashi!");
+    public void sha256_hash() throws Exception {
+        String input = "This is a string that we want to hash";
+
+
+        ByteArrayInputStream dataInputStream = new ByteArrayInputStream(input.getBytes("UTF-8"));
+
+        MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+
+
+        DigestInputStream in = new DigestInputStream(dataInputStream, messageDigest);
+
+        byte[] buffer = new byte[8192];
+        while (in.read(buffer) != -1) ;
+
+        byte[] raw = messageDigest.digest();
+
+
+        out.println("Digest Length : " + messageDigest.getDigestLength());
+        out.println("Algorithm     : " + messageDigest.getAlgorithm());
+
+
+        String base64Hash = new String(Base64.getEncoder().encode(raw));
+
+        out.println("base64        : " + base64Hash);
+
+        String hex = DatatypeConverter.printHexBinary(raw).toLowerCase();
+        out.println("hex           : " + hex);
 
     }
 }
