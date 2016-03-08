144b05bf3d4a29519646ba07ea5b4fd5f348740e
Jim Barritt <jim@planet-ix.com>
2016-03-08 15:37:06 +0000

    Adding http support

---
+++ src/test/java/jmdbtutorial/crypto/certificatetransparency/Test_Validate_Signed_Tree_Head.java
@@ -0,0 +1,19 @@
+package jmdbtutorial.crypto.certificatetransparency;
+
+import org.junit.Test;
+
+/**
+ * https://tools.ietf.org/html/rfc6962#section-4.3
+ * https://tools.ietf.org/html/rfc5246#section-4.7
+ * https://www.imperialviolet.org/2013/08/01/ctpilot.html
+ */
+public class Test_Validate_Signed_Tree_Head {
+
+    @Test
+    public void validateSignedTreeHead() {
+        String logRoot = "http://ct.googleapis.com/";
+        String sthPath = "pilot/ct/v1/get-sth";
+
+
+    }
+}
